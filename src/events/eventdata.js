b92a1939aad727301b7ea2c192c3b5c47a784cad
Adarsh Gupta <adarshguptamalawar@gmail.com>
2024-07-05 17:40:21 +0530

    eventdatachanges

--- src/events/eventdata.js
+++ src/events/eventdata.js
@@ -23,27 +23,27 @@ import image20 from  "../events/tedx4.jpg"
 let events = [
     {
       title: "CYGNUS",
-      description: " Ojaswa at SGSITS, Indore, hosted Cygnus: A Literary Journey, a lively festival inviting participants to explore and shape their literary interests. Events like LitClash featured persuasive skills, Escape College tested problem-solving, Story Guessing sparked creativity, and Open Mic Eve celebrated diverse expressions, fostering a welcoming literary community. Cygnus 22-23 wasn't just a festival; it was a laid-back celebration of literature.Cygnus 22-23 wasn't just a festival; it was a relaxed and joyful celebration of literature that brought together enthusiasts from all walks of life, making it an unforgettable experience.",
+      description: "Club Ojaswa at SGSITS, Indore, hosted Cygnus: A Litrophilic Saga, a lively literature festival inviting participants to explore and shape their literary interests. Events like LitClash featured persuasive skills, Escape College tested problem-solving, Story Guessing sparked creativity, and Open Mic Eve celebrated diverse expressions, fostering a welcoming literary community. Cygnus'23 wasn't just a festival; it was a laid-back celebration of literature.",
       images: [image1, image2, image3,image4],
     },
     {
       title: "MR. & MS. AAYAAM",
-      description: "Mr. & Ms. Aayaam, was an exuberant event celebrating inner personas and vibrant personalities. It consisted of three striking stages: nurturing tasks to showcase individual strengths, a bidding war of emotions challenging wit and charm, and a glamorous ramp showcase of traits.The final stage was a glamorous ramp walk where participants exhibited their traits, confidence, and style, captivating the audience with their poise and charisma. Mr. & Ms. Aayaam provided a platform for individuals to celebrate their uniqueness and unleash their personalities in a mesmerizing showcase of talent, leaving a lasting impression on all who attended.",
+      description: "Mr. & Ms. Aayaam, was an exuberant event celebrating inner personas and vibrant personalities. It consisted of three striking stages: Task It Up, nurturing tasks to showcase individual strengths, AndBeyond, a bidding war of emotions challenging wit, andKeep It Candid, a glamorous ramp showcase of traits. Mr. & Ms. Aayaam provided a platform to celebrate individuality and unleash the power of personalities in a mesmerizing showcase of talent.",
       images: [image5,image6,image7,image8],
     },
     {
       title: "JUDICIA: Unveiling Legal Mysteries",
-      description: "All Legal, All Wise Judicia, held on 18-19 November 2022, was a legal expedition challenging participants to unravel mysteries using wisdom and astuteness. It simulated a real courtroom, fostering a platform for legal processes and trials. This miniature courtroom experience was a resounding success, focusing on students from various branches and garnering attention from staff and faculties. Judicia extended beyond participants, fostering a shared understanding of legal processes converging mysteries and success.Judicia extended beyond the participants, fostering a shared understanding of legal procedures and the complexities of the judicial system",
+      description: "Judicia, held on 18-19 November 2022, was a legal expedition challenging participants to unravel mysteries using wisdom and astuteness. It simulated a real courtroom, fostering a platform for legal processes and trials. This miniature courtroom experience was a resounding success, focusing on students from various branches and garnering attention from staff and faculties. Judicia extended beyond participants, fostering a shared understanding of legal processes converging mysteries and success.",
       images: [image9,image10,image11,image12],
     },
     {
       title: "BATTLE OF INTELLECTS: Unleash Your Mind ",
-      description: "t was an intellectually stimulating competition. It started with a written test in Outwit the Devil,followed by teamwork and courage challenges in Outplay to Survive, and culminated in a dramatic final stage Outsmart Them All,where participants showcased oratory skills. This event tested and honed participants minds, ensuring that brilliance prevailed.The Battle of Intellects was not just a competition but a celebration of mental agility and collaborative effort, making it an event that participants and spectators alike found profoundly enriching and inspiring.",
+      description: "It was an intellectually stimulating competition. It started with a written test in Outwit the Devil, followed by teamwork and courage challenges in Outplay to Survive,and culminated in a dramatic final stage Outsmart Them All,where participants showcased oratory skills. This event tested and honed participants' minds, ensuring that brilliance prevailed.",
       images: [image13,image14,image15,image16],
     },
     {
       title: "TEDxSGSITS 2019",
-      description: "In 2019, TEDxSGSITS hosted a captivating one-day event featuring esteemed speakers such as Chetna Joshi, Aakash Ranison, Girish Agarwal, Parvinder Singh, Prachi Agrawal, and Anas Khan, who shared their insightful ideas. Our focus centered on the diverse student body across various branches of our college. These compelling talks fostered a shared exploration of ideas and knowledge.TEDxSGSITS was where minds connected, ideas flourished, and a sense of community and intellectual curiosity was cultivated. This event left a lasting impression on all attendees, providing them with valuable insights and inspiration for their future endeavors. ",
+      description: "In 2019, TEDxSGSITS hosted a captivating one-day event featuring esteemed speakers such as Chetna Joshi, Aakash Ranison, Girish Agarwal, Parvinder Singh, Prachi Agrawal, and Anas Khan, who shared their insightful ideas. Our focus centered on the diverse student body across various branches of our college. These compelling talks fostered a shared exploration of ideas and knowledge. TEDxSGSITS - where minds connect and ideas flourish.",
       images: [image17,image18,image19,image20],
     },
   ];
